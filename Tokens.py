0464014ebe3d2fec41f3b374754913b3bf41515c
Hilit Oreny <hilit.oreny@gmail.com>
2023-03-04 20:20:53 +0200

    Creating all 44 tokens

--- Tokens.py
+++ Tokens.py
@@ -10,22 +10,52 @@ class Token:
         print("The result of this token is: ", self.result)
 
 
-# creating all numerical tokens first
+# creating all 44 tokens
 
-Token("1", "1")
-Token("1", "1")
-Token("0", "0")
-Token("0", "0")
-Token("0", "0")
-Token("-1", "-1")
-Token("-1", "-1")
-Token("-1", "-1")
-Token("-2", "-2")
-Token("-2", "-2")
-Token("-3", "-3")
-Token("-3", "-3")
-Token("-4", "-4")
-Token("-4", "-4")
-Token("-5", "-5")
-Token("-6", "-6")
-Token("-8", "-8")
+token_pool = [Token("1", "1"),
+              Token("1", "1"),
+              Token("1", "1"),
+              Token("0", "0"),
+              Token("0", "0"),
+              Token("0", "0"),
+              Token("0", "0"),
+              Token("-1", "-1"),
+              Token("-1", "-1"),
+              Token("-1", "-1"),
+              Token("-1", "-1"),
+              Token("-1", "-1"),
+              Token("-2", "-2"),
+              Token("-2", "-2"),
+              Token("-2", "-2"),
+              Token("-2", "-2"),
+              Token("-3", "-3"),
+              Token("-3", "-3"),
+              Token("-3", "-3"),
+              Token("-4", "-4"),
+              Token("-4", "-4"),
+              Token("-5", "-5"),
+              Token("-5", "-5"),
+              Token("-6", "-6"),
+              Token("-7", "-7"),
+              Token("-8", "-8"),
+              Token("Skull", "Scenario Based"),
+              Token("Skull", "Scenario Based"),
+              Token("Skull", "Scenario Based"),
+              Token("Skull", "Scenario Based"),
+              Token("Cultist", "Scenario Based"),
+              Token("Cultist", "Scenario Based"),
+              Token("Cultist", "Scenario Based"),
+              Token("Cultist", "Scenario Based"),
+              Token("Elder Thing", "Scenario Based"),
+              Token("Elder Thing", "Scenario Based"),
+              Token("Elder Thing", "Scenario Based"),
+              Token("Elder Thing", "Scenario Based"),
+              Token("Tablet", "Scenario Based"),
+              Token("Tablet", "Scenario Based"),
+              Token("Tablet", "Scenario Based"),
+              Token("Tablet", "Scenario Based"),
+              Token("Elder Sign", "Reference the investigator's ability"),
+              Token("Auto-fail", "Automatic failure")]
+
+token_pool[42].drawing()
+token_pool[42].present_result()
