e9c450145af2f9ea81951d2409b3b79c846da9e3
Hilit Oreny <hilit.oreny@gmail.com>
2023-03-08 20:48:20 +0200

    Subclasses of Token

--- Tokens.py
+++ Tokens.py
@@ -7,30 +7,38 @@ class Token:
         print("A {} token was drawn from the bag".format(self.name))
 
     def present_result(self):
-        print("The result of this token is: ", self.result)
+        print("The result of this token is:", self.result)
 
 
-# creating all 44 tokens
+class PlusOneToken(Token):
+    def __init__(self):
+        super().__init__(name="1", result="1")
 
-token_pool = [Token("1", "1"),
-              Token("1", "1"),
-              Token("1", "1"),
-              Token("0", "0"),
-              Token("0", "0"),
-              Token("0", "0"),
-              Token("0", "0"),
-              Token("-1", "-1"),
-              Token("-1", "-1"),
-              Token("-1", "-1"),
-              Token("-1", "-1"),
-              Token("-1", "-1"),
-              Token("-2", "-2"),
-              Token("-2", "-2"),
-              Token("-2", "-2"),
-              Token("-2", "-2"),
-              Token("-3", "-3"),
-              Token("-3", "-3"),
-              Token("-3", "-3"),
+
+class ZeroToken(Token):
+    def __init__(self):
+        super().__init__(name="0", result="0")
+
+
+class MinusOneToken(Token):
+    def __init__(self):
+        super().__init__(name="-1", result="-1")
+
+
+class MinusTwoToken(Token):
+    def __init__(self):
+        super().__init__(name="-2", result="-2")
+
+class MinusThreeToken(Token):
+    def __init__(self):
+        super().__init__(name="-3", result="-3")
+
+
+token_pool = [PlusOneToken(), PlusOneToken(), PlusOneToken(),
+              ZeroToken(), ZeroToken(), ZeroToken(), ZeroToken(),
+              MinusOneToken(), MinusOneToken(), MinusOneToken(), MinusOneToken(), MinusOneToken(),
+              MinusTwoToken(), MinusTwoToken(), MinusTwoToken(), MinusTwoToken(),
+              MinusThreeToken(), MinusThreeToken(), MinusThreeToken(),
               Token("-4", "-4"),
               Token("-4", "-4"),
               Token("-5", "-5"),
@@ -57,5 +65,4 @@ token_pool = [Token("1", "1"),
               Token("Elder Sign", "Reference the investigator's ability"),
               Token("Auto-fail", "Automatic failure")]
 
-token_pool[42].drawing()
-token_pool[42].present_result()
+token_pool[16].drawing()
